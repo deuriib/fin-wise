97e6e820fa86a88d8a668d5fd005bcb9f690d7c4
Deuri Vasquez <deuriib@gmail.com>
2025-08-24 22:24:30 +0000

    centralize the logo vertically with the "FinWise" title

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -12,7 +12,7 @@ export function FinWiseLogo(props: React.SVGProps<SVGSVGElement>) {
           ".logo-text { font-family: 'Playfair Display', serif; font-weight: 700; font-size: 38px; fill: currentColor; }"
         }
       </style>
-      <g>
+      <g transform="translate(0, 2)">
         <g fill="hsl(var(--accent))">
           <path d="M10 15 a 5 5 0 0 1 5 -5 H 35 a 5 5 0 0 1 5 5 V 40 a 5 5 0 0 1 -5 5 H 15 a 5 5 0 0 1 -5 -5 Z" />
           <path d="M10 20 H 40 V 15 a 5 5 0 0 0 -5 -5 H 15 a 5 5 0 0 0 -5 5 Z" />
